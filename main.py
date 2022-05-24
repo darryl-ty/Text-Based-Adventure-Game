2eac3ded7dee858f8009a438ba54b6e797783bb8
darryl-ty <98935440+darryl-ty@users.noreply.github.com>
2022-05-23 22:39:00 -0400

    Partially vetted input code from user during character creation.

--- main.py
+++ main.py
@@ -37,21 +37,28 @@ def welcome_message():
 
 
 def character_create():
-    gender = input('Are you male or female? ')
-    if gender.lower().strip() != 'male' or gender.lower().strip() != 'female':
-        gender = input(f'\nPlease enter a valid gender: Male or Female\n')
+    gender = input('Are you male or female? \n')
+    if str(gender.lower().strip()) != 'male' or str(gender.lower().strip()) != 'female':
+        gender = input(f'Please enter a valid gender: Male or Female\n')
 
-    name = input('\nOkay next, what is your name? ')
+    name = input('Okay next, what is your name? \n')
     try:
-        age = int(input('\nHow old are you? \nNote: This game is NOT for users 17 years or younger: '))
+        age = int(input('Note: This game is NOT for users 17 years or younger\nHow old are you? \n'))
         if age <= 17:
-            age = int(input('Please enter an age of 18 years or older: '))
-        strength = int(input('\nOn a scale of 1 - 10, how strong are you? '))
-        agility = int(input('\nSimilarly, on a scale of 1 - 10, how agile are you? '))
-        toughness = int(input('\nLastly, on a scale of 1 - 10, how tough are you? '))
+            age = int(input('Please enter an age of 18 years or older: \n'))
+        strength = int(input('On a scale of 1 - 10, how strong are you? \n'))
+        if strength > 10 or strength < 1:
+            strength = int(input('Please enter a value between 1 - 10 \n'))
+        agility = int(input('Similarly, on a scale of 1 - 10, how agile are you? \n'))
+        if agility > 10 or agility < 1:
+            agility = int(input('Please enter a value between 1 - 10 \n'))
+        toughness = int(input('Lastly, on a scale of 1 - 10, how tough are you? \n'))
+        if toughness > 10 or toughness < 1:
+            toughness = int(input('Please enter a value between 1 - 10 \n'))
+
     except ValueError:
         print("Please enter a valid number.\n")
-        return game_start()
+        return character_create()
 
     return Character(name, age, gender, strength, agility, toughness, True)
 
